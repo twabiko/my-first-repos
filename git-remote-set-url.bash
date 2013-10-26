0598dcf462f2c29423c362c32d68329406a7f361
root <root@localhost.localdomain>
2013-10-26 13:19:33 +0900

    add config attrs.

--- git-remote-set-url.bash
+++ git-remote-set-url.bash
@@ -1,3 +1,5 @@
 #!/bin/bash -x
 git remote set-url origin https://twabiko@github.com/twabiko/my-first-repos.git
+git config --global user.name "twabiko"
+#git config --global user.email you@example.com
 exit 0
